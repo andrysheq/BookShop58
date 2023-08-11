3ccb99c6256b9feaae88068b894b4ee631d27e6f
Andrey Kareev <andrey.andreych@bk.ru>
2023-08-11 21:32:18 +0300

    added reviews design, made changes to navigation

---
+++ app/src/main/java/com/example/mystore/model/Cart.java
@@ -0,0 +1,14 @@
+package com.example.mystore.model;
+
+import com.example.mystore.model.Book;
+import com.example.mystore.model.BookInCart;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class Cart {
+    public static ArrayList<BookInCart> cart = new ArrayList<>();
+
+}
